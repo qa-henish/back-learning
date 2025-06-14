fd7553f6d7c35fe978015de43c076af5c6757345
reactguysinfotech <reactguys.infotech@gmail.com>
2025-06-14 15:29:19 +0530

    #Done custom ApiError & ApiResponse midddlewares

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => console.error("MongoDB Connection Error", error));
