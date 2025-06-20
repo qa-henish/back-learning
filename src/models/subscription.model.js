833d6e91b3854a4ac0edeaf4bb5a958990ebb9f0
qa-henish <henish.webosmotic@gmail.com>
2025-06-20 09:54:24 +0530

    Add subscription schema with subscriber and channel fields

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,5 +1,17 @@
 import mongoose, { Schema } from "mongoose";
 
-const subscriptionSchema = new Schema({});
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, // One who is subscribing
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId, // The channel being subscribed to
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema);
